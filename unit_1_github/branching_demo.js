2616b9e6e198f3307df8ed1c6dac0ea7ac37d106
Figsups <saksatti@gmail.com>
2024-09-09 12:14:08 -0400

    add currentDate

--- unit_1_github/branching_demo.js
+++ unit_1_github/branching_demo.js
@@ -6,5 +6,9 @@ const date = new Date();
 
 // getmonth() returns the month as an integer [0 - 11], Jan = 0, Dec = 11
 let month = months[date.getMonth()];
+let day = date.getDate();
+let year = date.getFullYear();
 
-console.log("The month is: " + month);
+let currentDate = "The current date is: " + month + " " + day + ", " + year;
+
+console.log(currentDate);
